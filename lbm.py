2a77638ca9322df812df06259d9a62c3e956b343
Luisper2 <luisper2@nmsu.edu>
2025-06-25 00:42:33 -0600

    v3

--- lbm.py
+++ lbm.py
@@ -1,30 +1,40 @@
 import os
 
+import jax
 import jax.numpy as np
+import jax.random as jr
 from tqdm import tqdm
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
 class LBM():
-    def __init__(self, nx, ny, tau = 1, reynolds = 80, perturbations = False):
+    def __init__(self, nx, ny, tau = 1, u0 = 0.1, perturbations = False):
         self.nx = nx # Width
         self.ny = ny # Height
 
-        self.tau = tau  # Cinematic Viscosity (Collision)
-        self.u0 = reynolds # Initial Velocity
-
-        # self.nt = 4000 # Number of Iterations
+        self.tau = tau  # Kinematic Viscosity (Collision)
+        self.u0 = u0 # Initial Velocity
 
         self.n_velocities = 9 # Number of Velocities
 
         self.x, self.y = np.meshgrid(np.arange(self.nx), np.arange(self.ny), indexing='ij') # Grid
 
         self.weight = np.zeros(self.n_velocities)  # Weight
+        # self.wt
 
-        self.lattive_u = np.zeros(self.n_velocities, dtype=int) # Lattive X Velocities
-        self.lattive_v = np.zeros(self.n_velocities, dtype=int) # Lattive Y Velocities
+        self.lattice = np.zeros((self.nx, self.ny)) # Lattice
+        
+        self.lattive_u = np.zeros(self.n_velocities, dtype=int) # Lattice X Velocities
+        self.lattive_v = np.zeros(self.n_velocities, dtype=int) # Lattice Y Velocities
+        # self.ex, self.ey
+
+        self.u = self.lattice.copy() # X Velocities
+        self.v = self.lattice.copy() # Y Velocities
 
         self.bounce = np.zeros(self.n_velocities, dtype=int) # Bounce
+        # self.bounce_back
+
+        self.density = np.ones((self.nx, self.ny))
 
         if perturbations:
             self.F = np.ones((self.ny, self.nx, self.n_velocities)) + 0.01 * jr.normal(jr.PRNGKey(0), (self.ny, self.nx, self.n_velocities))
@@ -34,11 +44,19 @@ class LBM():
         self.Feq = np.zeros((self.n_velocities, self.nx, self.ny))
         self.Ferr = self.Feq.copy()
 
+        self.cache = self.Feq.copy()
+
         self.boundary = np.zeros((self.nx, self.ny))
 
         self.__init_D2Q9__()
 
     def __init_D2Q9__(self):
+        # 6   2   5
+        #   \ | /
+        # 3 - 0 - 1
+        #   / | \
+        # 7   4   8
+
         self.weight.at[0].set(4 / 9)
         self.weight.at[1:5].set(1 / 9)
         self.weight.at[5:].set(1 / 36)
@@ -57,10 +75,38 @@ class LBM():
                 .at[np.array([4,7,8])].set(-1)
         )
 
-        self.bounce = [0, 3, 4, 1, 2, 7, 8, 5, 6]
+        self.bounce = np.array([0, 3, 4, 1, 2, 7, 8, 5, 6])
+
+        self.lattice = (
+            self.lattice
+            .at[:,  0].set(True)   # Left
+            .at[:, -1].set(True)   # Right
+            .at[0,  :].set(True)   # Down
+            .at[-1, :].set(True)   # Up
+        )
+
+    def update_equilibrium_distribution(self):
+        unique = self.u ** 2 + self.v ** 2
+
+        u_e = self.u[:, :, np.newaxis] * self.lattive_u[np.newaxis, np.newaxis, :]
+        v_e = self.v[:, :, np.newaxis] * self.lattive_v[np.newaxis, np.newaxis, :]
+
+        feq = (self.density[:, :, np.newaxis] * self.weight[np.newaxis, np.newaxis, :]) * (
+                1.0 + 3.0 * (u_e + v_e) + 4.5 * (u_e + v_e) ** 2 - 1.5 * unique[:, :, np.newaxis]
+        )
+
+        # Assign the result to self.feq
+        self.feq = feq.transpose(2,0,1)
+
+    def run(self, steps = 1000, save = 10):
+        for iter in tqdm(range(steps)):
+            simulation.update_equilibrium_distribution()
 
 if __name__ == '__main__':
+    jax.config.update("jax_enable_x64", True)
+
     simulation = LBM(400, 100)
+    simulation.run(10000, 10)
 
     cylinder_cx = 400 // 5
     cylinder_cy = 100 // 5
